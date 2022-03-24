96cb8837e5d1c031eaeba1fe9cfdeaa47763285d
ovie009 <ovieiffieu2much@gmail.com>
2022-03-25 00:03:56 +0100

    removed sroll sensitive nav temporarily

--- main.js
+++ main.js
@@ -225,30 +225,30 @@ function changeTheme() {
 }
 
 
-var lastScrollTop = 0;
-// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
-    var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-    let nav = document.querySelector("nav");
-    if (st > lastScrollTop && dropDown === false){
-        // downscroll code
-        //nav.style.position = "unset";
-        for (let i = 0; i < 70; i++) {
-            let inv = 70 - i;
-            nav.style.top = `-${i}px`;
-            nav.style.height = `${inv}px`;
-        }
-        //   console.log("scrolling down");
-    } else {
-        // upscroll code
-        for (let i = 0; i < 70; i++) {
-            let inv = 70 - i;
-            nav.style.top = `-${inv}px`;
-            nav.style.height = `${i}px`;
-        }
-        // console.log("scrolling up");
-    }
-    //if st is less than or equal to 0, let lastScrollTop be equal to 0, else let it be equal to st
-    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-}, false);
+// var lastScrollTop = 0;
+// // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+// window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+//     var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+//     let nav = document.querySelector("nav");
+//     if (st > lastScrollTop && dropDown === false){
+//         // downscroll code
+//         //nav.style.position = "unset";
+//         for (let i = 0; i < 70; i++) {
+//             let inv = 70 - i;
+//             nav.style.top = `-${i}px`;
+//             nav.style.height = `${inv}px`;
+//         }
+//         //   console.log("scrolling down");
+//     } else {
+//         // upscroll code
+//         for (let i = 0; i < 70; i++) {
+//             let inv = 70 - i;
+//             nav.style.top = `-${inv}px`;
+//             nav.style.height = `${i}px`;
+//         }
+//         // console.log("scrolling up");
+//     }
+//     //if st is less than or equal to 0, let lastScrollTop be equal to 0, else let it be equal to st
+//     lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+// }, false);
 
