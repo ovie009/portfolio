79f7a12fd6eb88e3e9d906648ca7d2d994eec714
ovie009 <ovieiffieu2much@gmail.com>
2022-03-24 23:44:38 +0100

    fix scroll sensitive nav when drop right is clicked v3

--- main.js
+++ main.js
@@ -225,17 +225,15 @@ function changeTheme() {
 }
 
 
-if ( dropDown === false ) {
-    
-    var lastScrollTop = 0;
-    // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-    window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
-       var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-       let nav = document.querySelector("nav");
-       if (st > lastScrollTop){
-          // downscroll code
-          //nav.style.position = "unset";
-          for (let i = 0; i < 70; i++) {
+var lastScrollTop = 0;
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+    var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+    let nav = document.querySelector("nav");
+    if (st > lastScrollTop && dropDown === false){
+        // downscroll code
+        //nav.style.position = "unset";
+        for (let i = 0; i < 70; i++) {
             let inv = 70 - i;
             nav.style.top = `-${i}px`;
             nav.style.height = `${inv}px`;
@@ -249,15 +247,8 @@ if ( dropDown === false ) {
             nav.style.height = `${i}px`;
         }
         // console.log("scrolling up");
-       }
-        //if st is less than or equal to 0, let lastScrollTop be equal to 0, else let it be equal to st
-       lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-    }, false);
+    }
+    //if st is less than or equal to 0, let lastScrollTop be equal to 0, else let it be equal to st
+    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
 
-} else {
-    let nav = document.querySelector("nav");
-    nav.style.top = `0px`;
-    nav.style.height = `70px`;
-    nav.style.position = `fixed`;
-    
-}
